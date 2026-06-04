d28d4b1a24289d441b3588a093ddd419c890b780
Ruth17M <manriquezruth2004@gmail.com>
2026-06-04 15:19:00 -0600

    Initial Commit

---
+++ QuePlan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QuePlan
+//
+//  Created by Ruth Manriquez on 04/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QuePlan/QuePlanApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuePlanApp.swift
+//  QuePlan
+//
+//  Created by Ruth Manriquez on 04/06/26.
+//
+
+import SwiftUI
+
+@main
+struct QuePlanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
